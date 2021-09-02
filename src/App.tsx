0eb4b053735d11200c9fad24891a538842c6ff01
kekeon <liuke095@gmail.com>
2021-09-02 23:59:27 +0800

    fix: add db icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import { renderRoutes } from 'react-router-config'
 import { RecoilRoot } from 'recoil'
+import { ConfigProvider } from 'antd'
+
 import './common/css/index.less'
+import './index.less'
 
 function App(props: any) {
-  return <RecoilRoot>{renderRoutes(props.route.routes)}</RecoilRoot>
+  return (
+    <ConfigProvider>
+      <RecoilRoot>{renderRoutes(props.route.routes)}</RecoilRoot>
+    </ConfigProvider>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 import { routes } from './routers/index'
-import './index.less'
-import '@/common/css/index.less'
 
 ReactDOM.render(<BrowserRouter>{renderRoutes(routes)}</BrowserRouter>, document.getElementById('root'))
