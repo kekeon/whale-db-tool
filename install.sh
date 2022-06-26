b412ecd710bef292c90ba4ddb23834113170370c
kekeon <liuke095@gmail.com>
2022-06-26 23:11:12 +0800

    feat: install bash

--- install.sh
+++ install.sh
@@ -32,31 +32,6 @@ detect_platform() {
   printf '%s' "${platform}"
 }
 
-detect_arch() {
-  local arch
-  arch="$(uname -m | tr '[:upper:]' '[:lower:]')"
-
-  case "${arch}" in
-    x86_64) arch="x64" ;;
-    amd64) arch="x64" ;;
-    armv*) arch="arm" ;;
-    arm64 | aarch64) arch="arm64" ;;
-  esac
-
-  # `uname -m` in some cases mis-reports 32-bit OS as 64-bit, so double check
-  if [ "${arch}" = "x64" ] && [ "$(getconf LONG_BIT)" -eq 32 ]; then
-    arch=i686
-  elif [ "${arch}" = "arm64" ] && [ "$(getconf LONG_BIT)" -eq 32 ]; then
-    arch=arm
-  fi
-
-  case "$arch" in
-    x64*) ;;
-    arm64*) ;;
-    *) return 1
-  esac
-  printf '%s' "${arch}"
-}
 
 download_and_install() {
   echo "install start whale db tool"
